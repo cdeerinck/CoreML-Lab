2e73950346ed04ae3b157aa067b9ca29c615e666
Chuck Deerinck <cjd9@me.com>
2019-07-16 10:43:20 -0700

    Initial Commit

---
+++ CoreML Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML Lab
+//
+//  Created by Chuck Deerinck on 7/16/19.
+//  Copyright © 2019 Chuck Deerinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
