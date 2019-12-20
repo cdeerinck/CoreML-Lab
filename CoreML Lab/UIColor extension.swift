e12e4582e8a9b31978066586f2f024c3459c47fe
Chuck Deerinck <cjd9@me.com>
2019-12-20 08:31:46 -0800

    All files

---
+++ CoreML Lab/UIColor extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor extension.swift
+//  CoreML Lab
+//
+//  Created by Chuck Deerinck on 7/18/19.
+//  Copyright © 2019 Chuck Deerinck. All rights reserved.
+//
+
+import UIKit
+
+infix operator ≅
+
+extension UIColor {
+    static func ≅ (lhs:UIColor, rhs:UIColor) -> Bool {
+        let tolerance:CGFloat = 0.05
+        var lhsRed:CGFloat=0, lhsGreen:CGFloat=0, lhsBlue:CGFloat=0, lhsAlpha:CGFloat=0
+        lhs.getRed(&lhsRed, green: &lhsGreen, blue: &lhsBlue, alpha: &lhsAlpha)
+        var rhsRed:CGFloat=0, rhsGreen:CGFloat=0, rhsBlue:CGFloat=0, rhsAlpha:CGFloat=0
+        rhs.getRed(&rhsRed, green: &rhsGreen, blue: &rhsBlue, alpha: &rhsAlpha)
+        //print()
+        //print (lhs, rhs, (abs(lhsRed - rhsRed) < tolerance) && (abs(lhsGreen - rhsGreen) < tolerance) && (abs(lhsBlue - rhsBlue) < tolerance) && (abs(lhsAlpha - rhsAlpha) < tolerance))
+        //print("red=", abs(lhsRed - rhsRed), "green=", abs(lhsGreen - rhsGreen),"blue=",abs(lhsBlue - rhsBlue),"alpha",abs(lhsAlpha - rhsAlpha))
+        return (abs(lhsRed - rhsRed) < tolerance) && (abs(lhsGreen - rhsGreen) < tolerance) && (abs(lhsBlue - rhsBlue) < tolerance) && (abs(lhsAlpha - rhsAlpha) < tolerance)
+    }
+}
